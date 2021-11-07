7352d371e91733488b389cfd498b8f197c36e29b
bartGAMER2001 <88946444+bartGAMER2001@users.noreply.github.com>
2021-11-07 15:10:22 -0600

    archivos arriba

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+
+<title>
+laravel-jr
+</title>
+
+</head>
+
+<body>
+<p>
+Bienvenido a mi primera Espinoza Zepeda Juan Ramon <i>página web</i>.
+<p>
+Espero se sientan bien con la prueba laravel
+</body>
+
+</html>
